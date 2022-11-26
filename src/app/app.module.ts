8970614db725c6c3ad9ad2f66bbf55e0d89c1425
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2022-11-26 13:06:26 -0300

    Criação e estilização do formulário no moment-form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
+import { MomentFormComponent } from './components/moment-form/moment-form.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { NewMomentComponent } from './components/pages/new-moment/new-moment.com
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    NewMomentComponent
+    NewMomentComponent,
+    MomentFormComponent
   ],
   imports: [
     BrowserModule,
